770e59ca14d2d67e88bbaa56052370cd42a5061b
Naresh <nareshvadlamani@convertcart.com>
2020-09-16 16:07:12 +0530

    first commit

---
+++ src/api/zone.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+const getZoneDataApi =
+  "https://qcaefqcyp9.execute-api.ap-south-1.amazonaws.com/prod/fetchzoneall";
+
+export const getZoneData = async () => {
+  try {
+    let data = await axios.get(getZoneDataApi);
+    return data;
+  } catch (error) {
+    return error;
+  }
+};
---
+++ src/modals/app/api.js
@@ -0,0 +1,26 @@
+export const baseUrl = "http://shark-api-v2.herokuapp.com/api";
+
+export const unitsGetAllUnitsListUrl = `https://qcaefqcyp9.execute-api.ap-south-1.amazonaws.com/prod/fetchunitsall`;
+
+export const rolesGetAllRolesListUrl = `${baseUrl}/role/fetch/all`;
+
+export const designationsGetAllDesignationsUrl = `${baseUrl}/designation/fetch/all`;
+
+export const equipmentGetAllEquipmentUrl = `${baseUrl}/equipment/fetch/all`;
+
+export const notificationsGetAllUrl = `https://qcaefqcyp9.execute-api.ap-south-1.amazonaws.com/prod/fetchnotification `;
+// http://shark-api-v2.herokuapp.com/api/notification/fetch/todays
+
+export const zonesGetAllZoneUrl = `https://qcaefqcyp9.execute-api.ap-south-1.amazonaws.com/prod/fetchzoneall`;
+
+export const hodGetAllHodUrl = `${baseUrl}/user/fetch/all/hod-users`;
+
+export const scheduleWipUrl = `${baseUrl}/wip/create`;
+
+export const updateWipUrl = `${baseUrl}/wip/update`;
+
+export const deleteWipUrl = `${baseUrl}/delete`;
+
+export const zoneGetWIPdetailsUrl = `${baseUrl}/wip/fetch/by-zone-id`;
+
+export const zoneUpdateWIPUrl = `${baseUrl}/wip/update`;
